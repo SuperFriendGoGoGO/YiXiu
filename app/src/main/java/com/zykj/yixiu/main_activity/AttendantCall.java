4adb6a87ca4e6eeb5c44dcc4a214946ad5321580
SuperFriendGoGoGO <1260377741@qq.com>
2017-04-13 15:18:44 +0800

    正式开始11

--- app/src/main/java/com/zykj/yixiu/main_activity/AttendantCall.java
+++ app/src/main/java/com/zykj/yixiu/main_activity/AttendantCall.java
@@ -35,7 +35,7 @@ public class AttendantCall extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.attendantcall);
+        setContentView(R.layout.main_attendantcall);
         ButterKnife.bind(this);
     }
 

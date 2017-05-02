f38fae7a34dd4bbec1ba20f9489b00db82b7bbb5
SuperFriendGoGoGO <1260377741@qq.com>
2017-05-02 10:14:34 +0800

    数据对接

--- app/src/main/java/com/zykj/yixiu/adapter/RvAdapter.java
+++ app/src/main/java/com/zykj/yixiu/adapter/RvAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.zykj.yixiu.R;
 import com.zykj.yixiu.widget.MyRVitem;
@@ -21,11 +23,17 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.Myholder> {
     @Override
     public Myholder onCreateViewHolder(ViewGroup parent, int viewType) {
       View view= View.inflate(context, R.layout.tool_rsview,null);
-        return null;
+        Myholder myholder=new Myholder(view);
+        return myholder;
     }
 
     @Override
     public void onBindViewHolder(Myholder holder, int position) {
+        holder.tv_zhonglei.setText(list.get(position).getTv_zhonglei());
+        holder.tv_address.setText(list.get(position).getTv_dizhi());
+        holder.tv_time.setText(list.get(position).getTv_time());
+        holder.tv_time2.setText(list.get(position).getTv_shijian());
+
 
     }
 
@@ -35,8 +43,18 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.Myholder> {
     }
 
     class Myholder extends RecyclerView.ViewHolder{
+        TextView tv_zhonglei,tv_time,tv_time2,tv_address;
+        Button bt_chongfa,bt_shanchu,bt_xiangqing;
         public Myholder(View itemView) {
             super(itemView);
+            tv_zhonglei= (TextView) itemView.findViewById(R.id.tv_zhonglei);
+            tv_time= (TextView) itemView.findViewById(R.id.tv_time);
+            tv_time2= (TextView) itemView.findViewById(R.id.tv_time2);
+            tv_address= (TextView) itemView.findViewById(R.id.tv_address);
+            bt_chongfa= (Button) itemView.findViewById(R.id.bt_chongfa);
+            bt_shanchu= (Button) itemView.findViewById(R.id.bt_shanchu);
+            bt_xiangqing= (Button) itemView.findViewById(R.id.bt_chakan);
+
         }
     }
 }
--- app/src/main/java/com/zykj/yixiu/widget/MyRVitem.java
+++ app/src/main/java/com/zykj/yixiu/widget/MyRVitem.java
@@ -8,75 +8,47 @@ import android.widget.TextView;
  */
 
 public class MyRVitem {
-    TextView tv_zhonglei,tv_dizhi,tv_shijian,tv_time;
-    Button bt_xingqing,bt_shanchu,bt_chongfa;
+   String tv_zhonglei,tv_dizhi,tv_shijian,tv_time;
 
     @Override
     public String toString() {
         return "MyRVitem{" +
-                "tv_zhonglei=" + tv_zhonglei +
-                ", tv_dizhi=" + tv_dizhi +
-                ", tv_shijian=" + tv_shijian +
-                ", tv_time=" + tv_time +
-                ", bt_xingqing=" + bt_xingqing +
-                ", bt_shanchu=" + bt_shanchu +
-                ", bt_chongfa=" + bt_chongfa +
+                "tv_zhonglei='" + tv_zhonglei + '\'' +
+                ", tv_dizhi='" + tv_dizhi + '\'' +
+                ", tv_shijian='" + tv_shijian + '\'' +
+                ", tv_time='" + tv_time + '\'' +
                 '}';
     }
 
-    public TextView getTv_zhonglei() {
+    public String getTv_zhonglei() {
         return tv_zhonglei;
     }
 
-    public void setTv_zhonglei(TextView tv_zhonglei) {
+    public void setTv_zhonglei(String tv_zhonglei) {
         this.tv_zhonglei = tv_zhonglei;
     }
 
-    public TextView getTv_dizhi() {
+    public String getTv_dizhi() {
         return tv_dizhi;
     }
 
-    public void setTv_dizhi(TextView tv_dizhi) {
+    public void setTv_dizhi(String tv_dizhi) {
         this.tv_dizhi = tv_dizhi;
     }
 
-    public TextView getTv_shijian() {
+    public String getTv_shijian() {
         return tv_shijian;
     }
 
-    public void setTv_shijian(TextView tv_shijian) {
+    public void setTv_shijian(String tv_shijian) {
         this.tv_shijian = tv_shijian;
     }
 
-    public TextView getTv_time() {
+    public String getTv_time() {
         return tv_time;
     }
 
-    public void setTv_time(TextView tv_time) {
+    public void setTv_time(String tv_time) {
         this.tv_time = tv_time;
-    }
-
-    public Button getBt_xingqing() {
-        return bt_xingqing;
-    }
-
-    public void setBt_xingqing(Button bt_xingqing) {
-        this.bt_xingqing = bt_xingqing;
-    }
-
-    public Button getBt_shanchu() {
-        return bt_shanchu;
-    }
-
-    public void setBt_shanchu(Button bt_shanchu) {
-        this.bt_shanchu = bt_shanchu;
-    }
-
-    public Button getBt_chongfa() {
-        return bt_chongfa;
-    }
-
-    public void setBt_chongfa(Button bt_chongfa) {
-        this.bt_chongfa = bt_chongfa;
     }
 }
